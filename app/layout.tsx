02b3862d75498c72272fc1852315af9f78c49b91
Nikhil-Pal-billionare <support@foxgen.in>
2026-06-10 10:57:12 +0530

    Integrate Clarity tracking script in layout
    Added Clarity tracking script to the RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -10,7 +11,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-[#080808] text-white">{children}</body>
+      <body className="min-h-screen bg-[#080808] text-white">{children}
+      <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wuljhk1k5o");
+      </script>
+      </body>
     </html>
   );
 }
