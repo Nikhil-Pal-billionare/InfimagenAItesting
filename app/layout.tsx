bd1ca544dd1651a605f49635f497ac3961e0b5da
Nikhil-Pal-billionare <support@foxgen.in>
2026-06-15 08:55:51 +0530

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     })(window, document, "clarity", "script", "wuljhk1k5o");
   `}
        </Script>
+       <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZX30ZJHGQN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZX30ZJHGQN');
+</script>
       </body>
     </html>
   );
